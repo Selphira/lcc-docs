bbf357620b71cee66e8836ff06c293855aca78ac
Riws <thibault.debruyne@hotmail.fr>
2025-02-21 15:57:13 +0100

    chore: move models + split

--- models/mod.py
+++ models/mod.py
@@ -1,28 +1,12 @@
-from dataclasses import dataclass
-from os import path as os_path
+from dataclasses import dataclass, field
 import re
-from unicodedata import normalize
 
-from settings import (
-    FLAG_DIR,
-    IMG_ROOT,
-    SITE_DIR,
-    Games,
-    attrs_icon_data,
-    domain_to_image,
-    image_data,
-)
+from models.url import Url
+from models.utils import slugify
+from settings import Games, attrs_icon_data
 
-
-def slugify(value: str) -> str:
-    """
-    Convert spaces to hyphens.
-    Remove characters that aren't alphanumerics, underscores, or hyphens.
-    Convert to lowercase. Also strip leading and trailing whitespace.
-    """
-    value = normalize("NFKD", value).encode("ascii", "ignore").decode("ascii")
-    value = re.sub(r"[^\w\s-]", "", value).strip().lower()
-    return re.sub(r"[-\s]+", "-", value)
+link_regex = re.compile(r"\[\[[^].]+\]\]")
+quote_regex = re.compile(r"`[^`]+`")
 
 
 @dataclass(slots=True, kw_only=True)
@@ -31,8 +15,14 @@ class Icon:
     label: str
 
 
-link_regex = re.compile(r"\[\[[^].]+\]\]")
-quote_regex = re.compile(r"`[^`]+`")
+class Category:
+    def __init__(self, name: str):
+        self.name = name
+        self.mods = list()
+
+    @property
+    def id(self) -> str:
+        return slugify(self.name)
 
 
 @dataclass(kw_only=True)
@@ -183,81 +173,3 @@ class Mod:
     @property
     def order_games(self) -> list:
         return [game for game in Games if game in self.games]
-
-
-class Category:
-    def __init__(self, name: str):
-        self.name = name
-        self.mods = list()
-
-    @property
-    def id(self) -> str:
-        return slugify(self.name)
-
-
-class Url:
-    domain_regex = re.compile(r"https?://(www\.)?(?P<domain>[^/]*).*")
-    country_image_suffix = "-flag-32.png"
-
-    def __init__(self, url):
-        self.url = url
-        self.img = None
-
-        img = self.get_image_name()
-        if img:
-            # cas spécial pour les drapeaux
-            if img.endswith(self.country_image_suffix):
-                img_data = image_data[self.country_image_suffix].copy()
-                img_data["title"] = img_data["title"] % img.removesuffix(
-                    self.country_image_suffix
-                )
-                dir = FLAG_DIR
-            else:
-                img_data = image_data.get(img, dict())
-                dir = SITE_DIR
-
-            img_dir = os_path.join("img", dir, img)
-            self.img = Image(src=img_dir, **img_data)
-
-    def get_image_special(self) -> str:
-        domain = self.get_domain()
-        img = domain_to_image.get(domain, "")
-
-        # check sous-domaine
-        if not img and domain.count(".") >= 2:
-            domain = domain.partition(".")[-1]
-            img = domain_to_image.get(domain, "")
-
-        return img
-
-    def get_tld(self) -> str:
-        return self.get_domain().rpartition(".")[-1]
-
-    def get_image_country(self) -> str:
-        country_img = f"{self.get_tld()}{self.country_image_suffix}"
-        img = ""
-        # auto-select
-        if os_path.exists(os_path.join(IMG_ROOT, FLAG_DIR, country_img)):
-            img = country_img
-
-        return img
-
-    def get_image_name(self) -> str:
-        return self.get_image_special() or self.get_image_country()
-
-    def get_domain(self) -> str:
-        domain = self.domain_regex.search(self.url).group("domain")
-        return domain or "localhost"
-
-    @property
-    def is_external(self) -> bool:
-        return self.url.startswith("http")
-
-
-@dataclass(slots=True, kw_only=True)
-class Image:
-    src: str
-    title: str = ""
-    alt: str = ""
-    width: int = 32
-    height: int = 32
