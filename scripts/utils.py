b840ab8e21a60f527278709502a21bb7cb93f780
Riws <thibault.debruyne@hotmail.fr>
2025-04-03 23:18:56 +0200

    feat(utils): add CleanModMixin/simplify_url/AUTHOR_PSEUDOS

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,8 @@
+from dataclasses import fields
 import json
 from pathlib import Path
+
+from models.mod import Mod
 
 
 class ModManager:
@@ -7,17 +10,48 @@ class ModManager:
     root = Path.cwd()
 
     @classmethod
-    def mods_path(cls) -> Path:
-        return cls.root / "db" / cls.mod_filename
+    def db_path(cls) -> Path:
+        return cls.root / "db"
 
     @classmethod
     def load(cls) -> dict:
-        with open(cls.mods_path(), "r", encoding="utf-8") as f:
+        with open(cls.db_path() / cls.mod_filename, "r", encoding="utf-8") as f:
             return json.load(f)
 
     @classmethod
     def export(cls, mods: dict) -> None:
         assert mods, "Mods not loaded"
 
-        with open(cls.mods_path(), "w", encoding="utf-8") as f:
+        with open(cls.db_path() / cls.mod_filename, "w", encoding="utf-8") as f:
             json.dump(mods, f, indent=4, ensure_ascii=False)
+
+
+class CleanModMixin:
+    def __init__(self, data: dict):
+        self.data = data
+        self.cleaned_data = dict()
+
+    def clean_all(self) -> None:
+        for field in fields(Mod):
+            attr = field.name
+            try:
+                cleaned_value = getattr(self, f"clean_{attr}")()
+            except AttributeError:
+                pass
+            else:
+                if cleaned_value is not None:
+                    self.cleaned_data[attr] = cleaned_value
+
+
+def simplify_url(url: str) -> str:
+    if url.startswith(("https://github.com/", "https://forums.beamdog.com/discussion/")):
+        url = "/".join(url.split("/")[:5])
+    return url.removesuffix("/")
+
+
+with open(ModManager.db_path() / "author_pseudos.json", "r", encoding="utf-8") as f:
+    author_pseudos = json.load(f)
+
+AUTHOR_PSEUDOS: dict[str, tuple[str]] = {
+    pseudo: k for k, pseudos in author_pseudos.items() for pseudo in pseudos
+}
